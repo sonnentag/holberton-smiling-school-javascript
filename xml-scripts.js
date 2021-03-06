1ed04aa10a7c8854aea08b6fc3a7e3061196bf93
Tony Sims <zocle@zocle.net>
2021-03-06 06:11:20 +0000

    pushed updates to fresh xml files

--- xml-scripts.js
+++ xml-scripts.js
@@ -7,23 +7,19 @@ function getMainId() {
 }
 
 // store json results directly to local storage
-function storeXml(carSection) {
+function storeJson(carSection) {
 	if (!sessionStorage.getItem(carSection)) {
-		xApiGetter(carSection);
+		jApiGetter(carSection);
 	}
 }
 
 // get json data from api based on main id
 function jApiGetter(carSection) {
   $.ajax({
-    url: 'https://smileschool-api.hbtn.info/xml/' + carSection,
+    url: 'https://smileschool-api.hbtn.info/' + carSection,
     method: 'GET',
-    dataType: 'xml',
-    success: function(xml) { 
- $xml = $( $.parseXML( xml ) );
- $xml.find("quotes").each(function(){
-  console.log($(this).find("name").text()); 
- });
+    success: (data) => { 
+	sessionStorage.setItem(carSection, JSON.stringify(data));
     },
 
     fail: () => { alert(`The backend is not reachable`); }
@@ -39,17 +35,83 @@ function buildMain(carSection, sData) {
 
   $(sData).each(function(i, item) {
 
-	  $(sData).find('quotes').each(function(){
-                    var name = $(this).find("name").text()
-                    alert(name);
-	  });
-	  })
+    const divTemplates = {
+      "quotes" :  '<div class="carousel-item"> <div class="row align-items-center justify-content-around"> <img class="quote-bg d-block ml-auto pl-2 py-2 rounded-circle" src="' + sData[i].pic_url + '" alt="slide' + sData[i].id++ + '"> <div class="col-12 col-sm-6 m-3 mr-auto"> <p>' + sData[i].text + '</p> <p class="mb-0">' + sData[i].name + '</p> <p><em>' + sData[i].title + '</em></p> </div> </div> </div>',
+      "latest-videos" : '<div class="carousel-item"><div class="card col-md-4 border-0"> <div class="d-flex tutorial-bg justify-content-center align-items-center"> <img src="./images/play.png" class="card-img-over" alt="Play button"> </div> <div class="card-body"> <h5 class="card-title font-weight-bold tutorials-h1">' + sData[i].title + '</h5> <p class="card-text tutorials-p">' + sData[i]["sub-title"] + '</p> <div class="row"> <img class="rounded-circle mx-3 tinythumb" src="' + sData[i].author_pic_url + '"> <p class="purple pt-1">' + sData[i].author + '</p> </div> <div class="row justify-content-between mt-1"> <div class="col"> <span class="score"> <div class="score-wrap"> <span class="stars-active"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> </span> <span class="stars-inactive"> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </span> </div> </span> </div> <p class="purple mr-3">' + sData[i].duration + '</p> </div> </div> </div> </div>',
+      "popular-tutorials" : '<div class="carousel-item"><div class="card col-md-4 border-0 my-auto"> <div class="d-flex tutorial-bg justify-content-center align-items-center"> <img src="./images/play.png" class="card-img-over" alt="Play button"> </div> <div class="card-body"> <h5 class="card-title font-weight-bold tutorials-h1">' + sData[i].title + '</h5> <p class="card-text tutorials-p">' + sData[i]["sub-title"] + '</p> <div class="row"> <img class="rounded-circle mx-3 tinythumb" src="' + sData[i].author_pic_url + '"> <p class="purple pt-1">' + sData[i].author + '</p> </div> <div class="row justify-content-between mt-1"> <div class="col"> <span class="score"> <div class="score-wrap"> <span class="stars-active"> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> <i class="fa fa-star" aria-hidden="true"></i> </span> <span class="stars-inactive"> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> <i class="fa fa-star-o" aria-hidden="true"></i> </span> </div> </span> </div> <p class="purple mr-3">' + sData[i].duration + '</p> </div> </div> </div> </div>'
+    }
 
+
+    let rating = sData[i].star * 20 + "%"; 
+    $("#empty" + carSection).append(divTemplates[carSection]);
+    $("#empty" + carSection + " .tutorial-bg").last().css("background-image", "url(" + sData[i].thumb_url + ")");
+    $(".stars-active").last().width(rating);
+  });
+console.log('#empty' + carSection);
+  $( "#empty" + carSection ).removeClass( "spinner-border" );
+
+  if (carSection !== 'quotes')
+ 	 $( "#empty" + carSection ).css('color', 'black');
+
+  $( "#empty" + carSection + " .sr-only" ).first().remove();
+  $( "#empty" + carSection ).removeAttr( "role" );
+  $( "#empty" + carSection + " .carousel-item" ).first().addClass( "active" );
 }
 
 // do it 
 $( document ).ready(function() {
 
-jApiGetter('quotes');
+  let mainId = getMainId();
+  if (mainId == 'homepage') {
+      storeJson('quotes');
+      storeJson('popular-tutorials');
+      storeJson('latest-videos');
+  }
+  if (mainId == 'pricing') {
+      storeJson('quotes');
+  }
+  if (mainId == 'courses') {
+      storeJson('courses');
+  }
+
+  $('.navbar-toggle').focus(function () { $.addClass('border border-warning'); });
+
+  setTimeout(function(){ 
+  if (mainId == 'homepage') {
+      buildMain('quotes', JSON.parse(sessionStorage.getItem('quotes'))); 
+      buildMain('popular-tutorials', JSON.parse(sessionStorage.getItem('popular-tutorials'))); 
+      buildMain('latest-videos', JSON.parse(sessionStorage.getItem('latest-videos'))); 
+    }
+  if (mainId == 'pricing') {
+      buildMain('quotes', JSON.parse(sessionStorage.getItem('quotes'))); 
+    }
+  if (mainId == 'courses') {
+      buildMain('courses', JSON.parse(sessionStorage.getItem('courses'))); 
+    }
+  }, 2000);
+
+
+$('#tutorials').carousel({
+  interval: 10000
+})
+
+$('#tutorials .carousel-item').each(function(){
+    var minPerSlide = 3;
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    
+    for (var i=0;i<minPerSlide;i++) {
+        next=next.next();
+        if (!next.length) {
+        	next = $(this).siblings(':first');
+      	}
+        
+        next.children(':first-child').clone().appendTo($(this));
+      }
+});
+
 
 });
